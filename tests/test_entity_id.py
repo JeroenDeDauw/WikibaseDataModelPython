b04ef2d2d543377ca9b3e46c77f1ad17a86989ab
jeroendedauw <jeroendedauw@gmail.com>
2013-08-10 09:06:58 +0200

    Add project base

---
+++ tests/test_entity_id.py
@@ -0,0 +1,8 @@
+import unittest
+from wikibase.dataModel.entity_id import EntityId
+
+
+class TestEntityId(unittest.TestCase):
+
+    def test_can_construct(self):
+        EntityId()
---
+++ wikibase/dataModel/entity_id.py
@@ -0,0 +1,2 @@
+class EntityId:
+    pass
