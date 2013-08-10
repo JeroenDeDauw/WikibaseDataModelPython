252f5dbeacad18907c0c3a140fe77d58ef7d13f8
jeroendedauw <jeroendedauw@gmail.com>
2013-08-10 15:30:45 +0200

    Made DataValue 'abstract'

--- dataValues/dataValue.py
+++ dataValues/dataValue.py
@@ -1,2 +1,14 @@
 class DataValue:
-    pass
+
+    def getType(self):
+        """
+        Returns the identifier of the datavalues type. This is a string.
+        """
+        raise NotImplementedError( "DataValues should implement this method" )
+
+    def getSortKey(self):
+        """
+        Returns a key that can be used to sort the data value with.
+        It can be either numeric or a string.
+        """
+        raise NotImplementedError( "DataValues should implement this method" )
--- tests/dataValues/test_data_value.py
+++ tests/dataValues/test_data_value.py
@@ -6,3 +6,11 @@ class TestDataValue(unittest.TestCase):
 
     def test_can_construct(self):
         DataValue()
+
+    def test_get_type_raises_error(self):
+        dv = DataValue()
+        self.assertRaises(NotImplementedError, dv.getType)
+
+    def test_get_sort_key_raises_error(self):
+        dv = DataValue()
+        self.assertRaises(NotImplementedError, dv.getSortKey)
