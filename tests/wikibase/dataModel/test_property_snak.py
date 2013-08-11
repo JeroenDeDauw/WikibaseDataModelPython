fde1e5e0350ad9af5d76e938d2adffdc74f887da
jeroendedauw <jeroendedauw@gmail.com>
2013-08-11 11:40:21 +0200

    Add propertyId field to propertySnak class

--- tests/wikibase/dataModel/test_property_snak.py
+++ tests/wikibase/dataModel/test_property_snak.py
@@ -1,9 +1,19 @@
 import unittest
 from tests.wikibase.dataModel.snak_test_case import SnakTestCase
+from wikibase.dataModel.entity_id import EntityId
 from wikibase.dataModel.property_snak import PropertySnak
 
 
 class TestPropertyValueSnak(unittest.TestCase, SnakTestCase):
 
     def new_instance(self):
-        return PropertySnak('value')
+        return PropertySnak('value', EntityId('property', 42))
+
+    def test_constructor_sets_fields(self):
+        snakType = 'somevalue'
+        propertyId = EntityId('property', 42)
+
+        snak = PropertySnak(snakType, propertyId)
+
+        self.assertEqual(snakType, snak.getType())
+        self.assertEqual(propertyId, snak.getPropertyId())
--- wikibase/dataModel/property_snak.py
+++ wikibase/dataModel/property_snak.py
@@ -3,8 +3,12 @@ from wikibase.dataModel.snak import Snak
 
 class PropertySnak(Snak):
 
-    def __init__(self, snakType):
+    def __init__(self, snakType, propertyId):
         self._snakType = snakType
+        self._propertyId = propertyId
 
     def getType(self):
         return self._snakType
+
+    def getPropertyId(self):
+        return self._propertyId
