d91dcc59fc097b36e24a9a620a4023c7e8919bbc
jeroendedauw <jeroendedauw@gmail.com>
2013-08-11 05:21:51 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='WikibaseDataModel',
+    version='0.0.1',
+    packages=[],
+    url='https://github.com/JeroenDeDauw/WikibaseDataModelPython',
+    license='GPL v2+',
+    author='Jeroen De Dauw',
+    author_email='jeroendedauw@gmail.com',
+    description='Implementation of the Wikibase DataModel in Python'
+)
