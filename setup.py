ef48cef31d864dc012876cac876ff689999d43d0
jeroendedauw <jeroendedauw@gmail.com>
2013-08-11 05:31:08 +0200

    Add classifiers field to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(
     license='GPL v2+',
     author='Jeroen De Dauw',
     author_email='jeroendedauw@gmail.com',
-    description='Implementation of the Wikibase DataModel in Python'
+    description='Implementation of the Wikibase DataModel in Python',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha'
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+y
+    ]
 )
