27deab36f67dcee871f532b2e1e58b6af0de6006
jeroendedauw <jeroendedauw@gmail.com>
2013-08-11 07:07:56 +0200

    Implemented booleanvalue.getSortKey

--- dataValues/booleanValue.py
+++ dataValues/booleanValue.py
@@ -3,8 +3,11 @@ from dataValues.dataValue import DataValue
 
 class BooleanValue(DataValue):
 
+    def __init__(self, value):
+        self._value = value
+
     def getType(self):
         return 'boolean'
 
     def getSortKey(self):
-        return 0 # TODO
+        return 1 if self._value else 0
--- tests/dataValues/test_boolean_value.py
+++ tests/dataValues/test_boolean_value.py
@@ -6,4 +6,9 @@ from tests.dataValues.data_value_test_case import DataValueTestCase
 class TestBooleanValue(unittest.TestCase, DataValueTestCase):
 
     def new_instance(self):
-        return BooleanValue()
+        return BooleanValue(True)
+
+    def test_get_sort_key_with(self):
+        self.assertEqual(BooleanValue(True).getSortKey(), 1)
+        self.assertEqual(BooleanValue(False).getSortKey(), 0)
+
