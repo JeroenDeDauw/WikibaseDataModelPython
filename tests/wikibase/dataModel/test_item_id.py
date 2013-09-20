df725781efe7d4d189082ccc7c7dd13d430787a5
jeroendedauw <jeroendedauw@gmail.com>
2013-09-20 15:57:21 +0200

    Add id format validation in ItemId

--- tests/wikibase/dataModel/test_item_id.py
+++ tests/wikibase/dataModel/test_item_id.py
@@ -17,3 +17,22 @@ class TestItemId(unittest.TestCase):
         itemId = ItemId(itemString)
         self.assertEqual(itemId.getSerialization(), itemString.upper())
         self.assertEqual(itemId.getEntityType(), 'item')
+
+    invalidIds = lambda: (
+        ('q 42', ),
+        ('42q', ),
+        ('Q0', ),
+        ('Q1000.', ),
+        ('Q1000q', ),
+        ('Q10.00', ),
+        ('Q', ),
+        ('', ),
+        (' ', ),
+        (' q42', ),
+        ('p42', ),
+    )
+
+    @data_provider(invalidIds)
+    def test_constructor_does_not_accept_invalid_id(self, invalidId):
+        doConstruct = lambda anId: ItemId(anId)
+        self.assertRaises(ValueError, doConstruct, invalidId)
--- wikibase/dataModel/item_id.py
+++ wikibase/dataModel/item_id.py
@@ -1,8 +1,14 @@
+import re
 from wikibase.dataModel.entity_id import EntityId
 
 
 class ItemId(EntityId):
 
     def __init__(self, serialization):
+        if re.match('^q[1-9][0-9]*$', serialization, re.IGNORECASE) is None:
+            raise ValueError
+
         super().__init__('item', serialization.upper())
+
+
 
